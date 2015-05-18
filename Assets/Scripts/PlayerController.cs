59ff2d89641616bc5bf7623887982ddb95d424d8
Ian <naibla8358@gmail.com>
2015-05-18 09:41:12 -0400

    Initialized the repository, with the correct files.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 10.0f;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		float vertical = Input.GetAxis("Vertical");
+		float horizontal = Input.GetAxis("Horizontal");
+
+		Vector3 moveBy = new Vector3(horizontal, 0, vertical);
+		Vector3 final = moveBy * this.speed * Time.deltaTime;
+
+		this.transform.Translate(final);
+	}
+}
