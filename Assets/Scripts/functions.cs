f0aeed5385acecbf8681b8c7a4d5544915784b7b
pawnWizard <ericlar@umich.edu>
2015-05-22 21:59:06 -0400

    Added function object, which will contain scripts that are not attached to any specific object

---
+++ Assets/Scripts/functions.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class functions : MonoBehaviour {
+
+	public Transform cube;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				Instantiate(cube, new Vector3(i, j, 0), Quaternion.identity);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	//void Update () {
+	
+	//}
+}
