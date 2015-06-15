289ff0dd5d150e9492c45042ef50024fa09cd02e
pawnWizard <ericlar@umich.edu>
2015-06-14 20:09:38 -0400

    Implemented grid, made cubes transparent, made queen piece

---
+++ Assets/queen_sphere.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class queen_sphere : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/queen_sphere_functions.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class queen_sphere_functions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+		// Initialize Queen piece
+/*		Instantiate (queen_sphere,
+		             new Vector3 (4, 4, 4),
+		             Quaternion.identity);
+
+*/	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
